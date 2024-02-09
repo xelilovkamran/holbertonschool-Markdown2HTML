17ff32416388ae8e0c576ba84e835b0de97e4a65
xelilovkamran <xlilovkamran@gmail.com>
2024-02-09 09:21:40 +0000

    fixed bugs

--- markdown2html.py
+++ markdown2html.py
@@ -1,15 +1,19 @@
 #!/usr/bin/python3
+'''Write a script markdown2html.py that takes an argument 2 strings:
+First argument is the name of the Markdown file
+Second argument is the output file name'''
+
 
 import sys
 from os.path import exists 
 
 if __name__ == "__main__":
     arguments = sys.argv
-    if len(arguments) < 2:
-        print("Usage: ./markdown2html.py README.md README.html")
+    if len(arguments) < 3:
+        sys.stderr.write("Usage: ./markdown2html.py README.md README.html")
         sys.exit(1)
-    elif not exists(f"./{arguments[1]}"):
-        print(f"Missing {arguments[1]}")
+    if not exists(f"./{arguments[1]}"):
+        sys.stderr.write(f"Missing {arguments[1]}")
         sys.exit(1)
-    else:
-        sys.exit(0)
+        
+    sys.exit(0)
