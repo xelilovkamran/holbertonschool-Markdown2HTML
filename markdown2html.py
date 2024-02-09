ca7b3b2e4e4dde436a76cf77f07bd3c1a298b523
xelilovkamran <xlilovkamran@gmail.com>
2024-02-09 10:39:54 +0000

    bug fix

--- markdown2html.py
+++ markdown2html.py
@@ -10,10 +10,10 @@ from os.path import exists
 if __name__ == "__main__":
     arguments = sys.argv
     if len(arguments) < 3:
-        sys.stderr.write("Usage: ./markdown2html.py README.md README.html ")
+        print("Usage: ./markdown2html.py README.md README.html ")
         sys.exit(1)
     if not exists(f"./{arguments[1]}"):
-        sys.stderr.write(f"Missing {arguments[1]} ")
+        print(f"Missing {arguments[1]} ")
         sys.exit(1)
         
     sys.exit(0)
